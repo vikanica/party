294cf25641280afe4adad857574dc0592d060b73
vikanica <32392416+vikanica@users.noreply.github.com>
2018-04-06 17:51:58 +0300

    awd

--- index.php
+++ index.php
@@ -27,16 +27,17 @@ class cat extends \atk4\data\Model {
 } */
 $db = new \atk4\data\Persistence_SQL('mysql:host=eu-mm-auto-dub-01-b.cleardb.net;dbname=heroku_d6f5fd68101f5d4;charset=utf8', 'b3484af324fedb','73d52b5043cbe42');
 class Friends extends \atk4\data\Model {
-  	public $table = 'party-b';
+    public $table = 'party-b';
 function init() {
-  	parent::init();
-  	$this->addField('name');
+    parent::init();
+    $this->addField('name');
     $this->addField('surname');
-    $this->addField('phone_number',['default'=>371]);
     $this->addField('email');
-  	//$this->addField('password',['type'=>'password']);
+    $this->addField('phone_number',['default'=>371]);
+    //$this->addField('password',['type'=>'password']);
     $this->addField('birthday',['type'=>'date']);
     $this->addField('notes', ['type'=>'text']);
+    $this->addField('age');
 }
 }
 $form = $app->layout->add('Form'); // создание формы
