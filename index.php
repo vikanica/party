84fc6e2b35fba288044ffd223843a5da809f6754
vikanica <32392416+vikanica@users.noreply.github.com>
2018-04-06 17:06:45 +0300

    X

--- index.php
+++ index.php
@@ -25,10 +25,9 @@ class cat extends \atk4\data\Model {
     $this->addField('price',['type'=>'money']);
   }
 } */
-$db = new
-\atk4\data\Persistence_SQL('mysql:dbname=main_db;host=localhost','root','');
+$db = new \atk4\data\Persistence_SQL('mysql:host=eu-mm-auto-dub-01-b.cleardb.net;dbname=heroku_d6f5fd68101f5d4;charset=utf8', 'b3484af324fedb','73d52b5043cbe42');
 class Friends extends \atk4\data\Model {
-  	public $table = 'friends';
+  	public $table = 'party-b';
 function init() {
   	parent::init();
   	$this->addField('name');
