a8ebb4ce0a51890911c0a3dce7ac82304f4a6c5f
vikanica <32392416+vikanica@users.noreply.github.com>
2018-04-06 17:23:32 +0300

    dfvg

--- admin.php
+++ admin.php
@@ -7,8 +7,7 @@ if (isset($_SESSION['admin_access'])) {
     $app = new \atk4\ui\App('Записи на тусовку');
     $app->initLayout('Admin');
 
-    $db = new
-    \atk4\data\Persistence_SQL('mysql:dbname=main_db;host=localhost','root','');
+  $db = new \atk4\data\Persistence_SQL('mysql:host=eu-mm-auto-dub-01-b.cleardb.net;dbname=heroku_d6f5fd68101f5d4;charset=utf8', 'b3484af324fedb','73d52b5043cbe42');
     class Friends extends \atk4\data\Model {
       	public $table = 'friends';
     function init() {
